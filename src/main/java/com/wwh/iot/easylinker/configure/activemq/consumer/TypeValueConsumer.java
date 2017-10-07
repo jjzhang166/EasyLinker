9b3fd819da8bef730fe725b198f726bff0a64f1d
Administrator <751957846@qq.com>
2017-10-07 10:04:58 +0800

    修复手残导致空值问题

--- src/main/java/com/wwh/iot/easylinker/configure/activemq/consumer/TypeValueConsumer.java
+++ src/main/java/com/wwh/iot/easylinker/configure/activemq/consumer/TypeValueConsumer.java
@@ -11,7 +11,7 @@ import org.apache.activemq.command.ActiveMQTextMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
-import java.lang.String;
+
 /**
  * Created by wwhai on 2017/7/31.
  */
@@ -26,45 +26,36 @@ public class TypeValueConsumer {
     String textMessage;
     String valueName;//计量单位 比如 牛顿 千克
     JSONObject receivedMessageJson;
-    boolean isLocalDevice=false;
 
     @JmsListener(destination = "device.typevalue.>")
-    public void receiveMessage(ActiveMQMessage message) throws Exception{
-        if (message.getConnection().getConnectionInfo().getClientIp().startsWith("tcp://127.0.0.1")){
-            isLocalDevice=true;
-        }
+    public void receiveMessage(ActiveMQMessage message) throws Exception {
+        System.out.println(message);
+        if (message instanceof ActiveMQTextMessage) {
+            receivedMessageJson = JSONObject.parseObject(((ActiveMQTextMessage) message).getText().toString());
+            username = receivedMessageJson.get("username").toString();
+            textMessage = receivedMessageJson.get("message").toString();
+            valueName = receivedMessageJson.get("name").toString();
 
-        if (message instanceof ActiveMQTextMessage){
-            receivedMessageJson=JSONObject.parseObject(((ActiveMQTextMessage) message).getText().toString());
-            username=receivedMessageJson.get("username").toString();
-            textMessage=receivedMessageJson.get("message").toString();
-            valueName=receivedMessageJson.get("name").toString();
-
-        }else if(message instanceof ActiveMQBytesMessage){
-            receivedMessageJson=JSONObject.parseObject(new String(message.getMessage().getContent().getData(),"utf-8"));
-            username=receivedMessageJson.get("username").toString();
-            textMessage=receivedMessageJson.get("message").toString();
-            valueName=receivedMessageJson.get("name").toString();
+        } else if (message instanceof ActiveMQBytesMessage) {
+            receivedMessageJson = JSONObject.parseObject(new String(message.getMessage().getContent().getData(), "utf-8"));
+            username = receivedMessageJson.get("username").toString();
+            textMessage = receivedMessageJson.get("message").toString();
+            valueName = receivedMessageJson.get("name").toString();
         }
-        if (username!=null){
-            Device device=deviceRepository.findOne(username);
-            if (device!=null){
-                TypeValueData typeValueData=new TypeValueData();
+        if (username != null && textMessage != null) {
+            Device device = deviceRepository.findOne(username);
+            if (device != null) {
+                TypeValueData typeValueData = new TypeValueData();
                 typeValueData.setDevice(device);
                 typeValueData.setValue(textMessage);
                 typeValueData.setName(valueName);
                 typeValueDataRepository.save(typeValueData);
-            }else {
-                if (isLocalDevice){
-                    System.out.println("Local Device !");
-                }else {
-                    System.out.println("Device Not Exist!");
-                }
-
+            } else {
+                    System.out.println("Local Device or no such device !");
             }
 
+        } else {
+            System.out.println("Null value!");
         }
-
-        System.out.println("From device value message:"+message);
     }
 }
